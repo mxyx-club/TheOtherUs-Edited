d31ebc20a797775c38bf6b91bddb87d99153d8ac
mxyx <goodnight412@163.com>
2024-05-09 10:53:32 +0800

    Update CosmeticsUnlocker

---
+++ TheOtherRoles/Patches/OtherPatch.cs
@@ -0,0 +1,13 @@
+﻿using TheOtherRoles.Modules;
+
+namespace TheOtherRoles.Patches
+{
+    [HarmonyPatch(typeof(HatManager), nameof(HatManager.Initialize))]
+    public static class HatManager_Initialize
+    {
+        public static void Postfix(HatManager __instance)
+        {
+            CosmeticsUnlocker.unlockCosmetics(__instance);
+        }
+    }
+}
